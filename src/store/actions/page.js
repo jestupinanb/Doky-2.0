d039544746aeac9ed33b178d353e72fad99685ea
jestupinanb <30636279+jestupinanb@users.noreply.github.com>
2020-01-31 23:06:59 -0500

    Eliminando archivos no necesarios

--- src/store/actions/page.js
+++
@@ -1,7 +0,0 @@
-import { SHOW_SIDE_DRAWER } from "../types/page"
-
-export const showSidedrawer = () =>{
-    return{
-        type:SHOW_SIDE_DRAWER
-    }
-}
--- src/store/types/page.js
+++
@@ -1,1 +0,0 @@
-export const SHOW_SIDE_DRAWER = 'SHOW_SIDE_DRAWER'
